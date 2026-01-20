95ea605859882892bb775d030b7834091d1462fb
Tjark School <tjarkschool@MacBook-Air-von-Tjark.local>
2026-01-20 11:54:12 +0100

    Fix circular dependency in eventemitter3 shim - use direct require path

--- src/lib/eventemitter3-shim.ts
+++ src/lib/eventemitter3-shim.ts
@@ -1,5 +1,8 @@
 // Shim for eventemitter3 to provide a proper default export
 // This fixes the "EVe is not a constructor" error in production builds
-import EventEmitter from 'eventemitter3';
+// We need to use require to avoid circular dependency with the alias
+// @ts-ignore
+const EventEmitterModule = require('../../node_modules/eventemitter3/index.js');
+const EventEmitter = EventEmitterModule.default || EventEmitterModule;
 export default EventEmitter;
 export { EventEmitter };
